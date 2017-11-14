00882809f88c130faaf3d77fed287d7b689a3acd
XavierPrudent <prudentxavier@gmail.com>
2017-11-14 12:10:21 -0500

    working version

--- R/include.R
+++ R/include.R
@@ -1,9 +1,38 @@
+#####################################################
+## User input data
+set.user.data <- function(){
+  gtfs  <<- "/Users/lavieestuntoucan/civ-sto/data/GTFS/sto/jan2017-july2017/"
+  chipCards <<- "/Users/lavieestuntoucan/civ-sto/data/cartes_a_puces/sto/destination_estimee/Deplacement2017JanFev.CSV"
+  
+  trip.tag <<- "HIV"
+  my.week  <<- c("Tuesday","Wednesday","Thursday")
+  my.week.tag <<- "Semaine"
+  output.dir <<- "/Users/lavieestuntoucan/civ-sto/tech/profil_de_charge/out/"
+  first.day <<- as.Date("2017-01-09")
+  last.day <<- as.Date("2017-02-26")
+}
 
 
 #####################################################
-cut.data.period <- function(){
-  cards <<- cards %>% filter(DateDeplacement >= first.day & DateDeplacement <= last.day) %>%
-    filter(weekday %in% my.week)
+read.gtfs <- function(){
+  trips <<- fread(paste0(gtfs,"trips.txt"))
+  routes <<- fread(paste0(gtfs,"routes.txt"))
+  stops <<- fread(paste0(gtfs,"stops.txt"))
+  stop_times <<- fread(paste0(gtfs,"stop_times.txt"))
+}
+#####################################################
+shape.cut.gtfs <- function(){
+  ## Analysis cuts
+  trips <<- trips %>% rename(route_var_id = route_id)
+  routes <<- routes %>% rename(route_var_id = route_id) %>% rename(route_id = route_short_name)
+  ## Data shaping
+  routes <<- routes %>% filter(route_id %in% routes.sto)
+  trips <<- trips %>% filter(route_var_id %in% routes$route_var_id & 
+                               grepl(my.week.tag,trip_id) &
+                               grepl(trip.tag, trip_id)) %>% 
+    left_join(routes,by="route_var_id")
+  stop_times <<- stop_times %>% filter(grepl(trip.tag, trip_id) & 
+                                         trip_id %in% trips$trip_id)
 }
 #####################################################
 read.cards <- function(){
@@ -14,89 +43,39 @@ read.cards <- function(){
 }
 #####################################################
 plot.freq <- function(){
-p <- plot_ly(data = cards , x = ~DateDeplacement, type = "histogram") %>%
-  layout(xaxis=list(type="date", tickformat="%d/%m", title="Date du déplacement"),
-         yaxis=list(title="Décompte"))
-p
-htmlwidgets::saveWidget(p, paste0(output.dir,"frequentation_jours.html"))
+  p <- plot_ly(data = cards , x = ~DateDeplacement, type = "histogram") %>%
+    layout(xaxis=list(type="date", tickformat="%d/%m", title="Date du déplacement"),
+           yaxis=list(title="Décompte"))
+  p
+  htmlwidgets::saveWidget(p, paste0(output.dir,"frequentation_jours.html"))
 }
-
 #####################################################
-read.gtfs <- function(){
-  trips <<- fread(paste0(gtfs,"trips.txt"))
-  routes <<- fread(paste0(gtfs,"routes.txt"))
-  stops <<- fread(paste0(gtfs,"stops.txt"))
-  stop_times <<- fread(paste0(gtfs,"stop_times.txt"))
+cut.data.period <- function(){
+  cards <<- cards %>% 
+    filter(DateDeplacement >= first.day & DateDeplacement <= last.day) %>%
+    filter(weekday %in% my.week)
 }
-
 #####################################################
-shape.cut.gtfs <- function(){
-  ## Analysis cuts
-  trips <<- trips %>% separate(route_id,sep="-",into=c("route_id","var"))
-  routes <<- routes %>% separate(route_id,sep="-",into=c("route_id","var"))
-  ## Data shaping
-  trips <<- trips %>% filter(route_id %in% routes.sto & 
-                                grepl(my.week.tag,trip_id) &
-                                grepl(trip.tag, trip_id))
-  routes <<- routes %>% filter(route_id %in% routes.sto)
-  stop_times <<- stop_times %>% filter(grepl(trip.tag, trip_id) & 
-                                          trip_id %in% trips$trip_id)
+frac.destOK <- function(){
+  n.all <- nrow(cards)
+  n.destOK <- cards %>% filter(!is.na(NumArretDebarquement1)) %>% nrow()
+  eff.destOK <<-  n.destOK / n.all
+  frac.cartes <<- 0.80
+  n.days <<- length(unique(cards$DateDeplacement))
 }
-
-#####################################################
-## For a given route, extract the variantes
-extract_variantes <- function(i.route_id){
-  list.var <- trips %>% filter(route_id == i.route_id) %>% dplyr::select(trip_headsign) %>% unique()
-  return(list.var)
-}
-
-#####################################################
-## For a given route and variante, extract the list of trips and their stop
-extract_stops <- function(i.route_id,i.var){
-  list.stops <- list()
-  list.trips <- trips %>% 
-    filter(route_id == i.route_id & trip_headsign == i.var) %>% 
-    dplyr::select(trip_id) %>% unique()
-  
-  ## Loop over trips
-  i <- 1
-  for( i.trip in list.trips$trip_id){
-    list.stops[[i]] <- stop_times %>% filter(trip_id == i.trip) %>% dplyr::select(stop_id)
-    i <- i +1
-  }
-  
-  ## Return only 1 list, the longest
-  list.stops <- unique(list.stops)
-  if( length(list.stops) > 1){
-    cnt <- sapply(list.stops, length)
-    list.stops <- list.stops[cnt == max(cnt)]
-  }
-  list.stops <- as.data.frame(list.stops)
-  
-  ## Append the stop name
-  list.stops <- list.stops %>% 
-    mutate(stop_id = as.character(stop_id) ) %>% 
-    left_join(stops,by="stop_id") %>% 
-    dplyr::select(stop_id,stop_name) %>%
-    mutate(stop_seq=row_number())
-  
-  return(list.stops)
-}
-
 #####################################################
 ## define a helper function
 empty_as_na <- function(x){
   if("factor" %in% class(x)) x <- as.character(x) ## since ifelse wont work with factors
   ifelse(as.character(x)!="", x, NA)
 }
-
 #####################################################
 ## Merge all trips info
 merge.trips.info <- function(){
   DateDeplacement <- c(cards$DateDeplacement,cards$DateDeplacement,cards$DateDeplacement,
                        cards$DateDeplacement,cards$DateDeplacement,cards$DateDeplacement,
-                       cards$DateDeplacement,cards$DateDeplacement,cards$DateDeplacement,cards$DateDeplacement) ## transform all columns to NA if empty
-  ## Empty as NA
+                       cards$DateDeplacement,cards$DateDeplacement,cards$DateDeplacement,cards$DateDeplacement) 
+  ## transform all columns to NA if empty
   cards <<- cards %>% mutate_all(funs(empty_as_na)) 
   ## append all trips
   HeureTransaction <- c(cards$HeureTransaction,cards$HeureTransaction2,cards$HeureTransaction3,
@@ -111,8 +90,173 @@ merge.trips.info <- function(){
   NumArretDebarquement <- c(cards$NumArretDebarquement1,cards$NumArretDebarquement2,cards$NumArretDebarquement3,
                             cards$NumArretDebarquement4,cards$NumArretDebarquement5,cards$NumArretDebarquement6,
                             cards$NumArretDebarquement7,cards$NumArretDebarquement8,cards$NumArretDebarquement9,cards$NumArretDebarquement10)
+  ## Make de dataframe
   cards <<- data.frame(DateDeplacement,HeureTransaction,NoLigne,NumArret,NumArretDebarquement)
   cards <<- cards %>% filter(!is.na(NumArretDebarquement))
+  ## Hour
+  cards <<- cards %>% 
+    separate(col=HeureTransaction, sep=":", into=c("Hres","Mins","Secs")) %>% 
+    mutate(weekday=weekdays(DateDeplacement)) %>%
+    mutate(HeureTransaction = round(as.numeric(Hres) + as.numeric(Mins)/60,digits=1)) %>% select(-c(Hres,Mins,Secs))
+}
+#####################################################
+## Count charge and plot profile per routes
+plot.charge.profiles <- function(){
+  ## Loop over the routes
+  for( i.route_id in routes.sto){
+    ##
+    ## Extract variantes
+    list.var <- extract_variantes(i.route_id)
+    for( i.var in list.var$trip_headsign){
+      ##
+      ## Count
+      df <- count.charge(i.route_id,i.var)
+      if( is.na(df)[1] ) next
+      ##
+      ## Plot the data and save
+      p <- create.plotly(df)
+      htmlwidgets::saveWidget(p, paste0(output.dir,"profilCharge_ligne",i.route_id,"_",i.var,".html"))
+    }
+  }
+}
+#####################################################
+create.plotly <- function(df){
+  p <- plot_ly(df, x = ~stop_id, y = ~mont, type = 'bar', name = 'Montées',hoverinfo = 'text',
+               text = ~paste('Arrêt : ', stop_id,
+                             '</br>Nom :', stop_name,
+                             '</br>Montées : ', mont)) %>%
+    add_trace(y = ~-des, name = 'Descentes', hoverinfo = 'text',
+              text = ~paste('Arrêt : ', stop_id,
+                            '</br>Nom :', stop_name,
+                            '</br>Descentes : ', des)) %>%
+    add_trace(y = ~charge, name = 'Charge', type='scatter', mode = 'lines+markers',
+              hoverinfo = 'text',
+              text = ~paste('Arrêt : ', stop_id,
+                            '</br>Nom :', stop_name,
+                            '</br>Charge : ', charge))  %>%
+    layout(margin = list(l=50, r=50, b=100, t=50, pad=0),
+           yaxis = list(title = 'Décompte'), xaxis = list(title = "Numéro de l'arrêt"))
+  return(p)
+}
+#####################################################
+## For a given route, extract the variantes
+extract_variantes <- function(i.route_id){
+  list.var <- trips %>% filter(route_id == i.route_id) %>% dplyr::select(trip_headsign) %>% unique()
+  return(list.var)
+}
+#####################################################
+count.charge <- function(i.route_id,i.var){
+  
+  cat(paste(i.route_id,i.var,"\n"))
+  ##
+  ## Extract the list of stops
+  list.stops <- extract_stops(i.route_id,i.var)
+  if( is.na(list.stops)[1]) return(NA)
+  if( nrow(list.stops) == 0) return(NA)
+  if( nrow(list.stops) != length(unique(list.stops$stop_id)) ) return(NA) # no loops
+  ##
+  ## Extract trips on this line and variant
+  i.cards <- cards %>% filter(NoLigne == i.route_id & 
+                                !is.na(NumArretDebarquement) &
+                                NumArret %in% list.stops$stop_id &
+                                NumArretDebarquement %in% list.stops$stop_id) %>% 
+    rowwise() %>% 
+    mutate(NumArretSeq = list.stops[which(list.stops$stop_id == NumArret),"stop_seq"],
+           NumArretDebarquementSeq = list.stops[which(list.stops$stop_id == NumArretDebarquement),"stop_seq"]) %>%
+    filter(NumArretDebarquementSeq > NumArretSeq)
+  ##
+  ## Number of in and out
+  df.stops.j <- list.stops %>% 
+    mutate(mont=NA,mont.tot=NA,des=NA,des.tot=NA,charge=NA) %>%
+    rowwise() %>% 
+    mutate(mont = length(which(i.cards$NumArret == stop_id)), 
+           des = length(which(i.cards$NumArretDebarquement == stop_id)))
+  ##
+  ## Scale up according to the algorithm efficiency
+  df.stops.j <- mutate(df.stops.j,  mont = mont / eff.destOK, des = des / eff.destOK)
+  ##
+  ## Scale up according to the fraction of trips with cards
+  df.stops.j <- mutate(df.stops.j,  mont = mont / frac.cartes, des = des / frac.cartes)
+  ##
+  ## Average down to the number of days included
+  df.stops.j <- mutate(df.stops.j,  mont = mont / n.days, des = des / n.days )
+  ##
+  ## Total loading
+  df.stops.j$mont <- round(df.stops.j$mont)
+  df.stops.j$des <- round(df.stops.j$des)
+  df.stops.j$mont.tot <- round(cumsum(df.stops.j$mont))
+  df.stops.j$des.tot <- round(cumsum(df.stops.j$des))
+  df.stops.j$charge <- round(df.stops.j$mont.tot - df.stops.j$des.tot)
+  df.stops.j$stop_id <- factor(df.stops.j$stop_id, levels = df.stops.j$stop_id)
+  
+  return(df.stops.j)
+}
+#####################################################
+## For a given route and variante, extract the list of trips and their stop
+extract_stops <- function(i.route_id,i.var){
+  list.stops <- list()
+  list.trips <- trips %>% 
+    filter(route_id == i.route_id & trip_headsign == i.var) %>% 
+    dplyr::select(trip_id) %>% unique()
+  if( nrow(list.trips) == 0) return(NA)
+  
+  ## Loop over trips
+  for( i in 1:nrow(list.trips) ){
+    list.stops[[i]] <- stop_times %>% 
+      filter(trip_id == list.trips$trip_id[i]) %>% 
+      dplyr::select(stop_id)
+  }
+  
+  ## Return only 1 list, the longest
+  list.stops <- unique(list.stops)
+  if( length(list.stops) > 1){
+    cnt <- sapply(list.stops, nrow)
+    list.stops <- list.stops[cnt == max(cnt)]
+  }
+  list.stops <- as.data.frame(list.stops)
+  if( nrow(list.stops) == 0) return(NA)
+  
+  ## Append the stop name
+  list.stops <- list.stops %>% 
+    mutate(stop_id = as.character(stop_id) ) %>% 
+    left_join(stops,by="stop_id") %>% 
+    dplyr::select(stop_id,stop_name) %>%
+    mutate(stop_seq=row_number())
+  
+  return(list.stops)
+}
+#####################################################
+count.md <- function(i.route_id,list.stops,md){
+  ##
+  ## Extract trips on this line for these stops
+  i.cards <- cards %>% filter(NoLigne == i.route_id & !is.na(NumArretDebarquement) )
+  if( md == "m" ) i.cards <- i.cards %>% filter(NumArret %in% list.stops)
+  if( md == "d" ) i.cards <- i.cards %>% filter(NumArretDebarquement %in% list.stops)
+  list.stops <- data.frame(stop_id=list.stops)
+  ##
+  ## Number of in and out
+  df.stops.j <- list.stops %>% 
+    mutate(mont=NA,des=NA) %>%
+    rowwise() %>% 
+    mutate(mont = length(which(i.cards$NumArret == stop_id)), 
+           des = length(which(i.cards$NumArretDebarquement == stop_id)))
+  ##
+  ## Scale up according to the algorithm efficiency
+  df.stops.j <- mutate(df.stops.j,  mont = mont / eff.destOK, des = des / eff.destOK)
+  ##
+  ## Scale up according to the fraction of trips with cards
+  df.stops.j <- mutate(df.stops.j,  mont = mont / frac.cartes, des = des / frac.cartes)
+  ##
+  ## Average down to the number of days included
+  df.stops.j <- mutate(df.stops.j,  mont = mont / n.days, des = des / n.days )
+  ##
+  ## Total loading
+  df.stops.j$mont <- round(df.stops.j$mont)
+  df.stops.j$des <- round(df.stops.j$des)
+  if( md == "m" ) df.stops.j <- select(df.stops.j,c(stop_id,mont))
+  if( md == "d" ) df.stops.j <- select(df.stops.j,c(stop_id,des))
+  colnames(df.stops.j)[2] <- "count"
+  return(df.stops.j)
 }
 
 
--- R/main.R
+++ R/main.R
@@ -1,26 +1,14 @@
 
+## Compute charge profile for STO lines heading to Ottawa downtown
+## Consider only a portion of Jan/Feb 2017 and average it to 1 day
+
 source("/Users/lavieestuntoucan/Civilia/tech/general/load_R_pkg.R")
 source("/Users/lavieestuntoucan/civ-sto/tech/profil_de_charge/R/include.R")
 
-## Difference entre 31 et 131, sachant qu'il existe des variantes pour la 31 seule
-## Pourquoi certains arrêts de ligne, contenus dans la BD, ne sont pas dans le gtfs? (67%)
-## pas de profile de charge pour les lignes avec 2fois le meme arrêt
+## donnes brutes cartes puces : montees, efficacité par ligne sur les arrêts d'ottawa?
 
-#################
 ## User input data
-gtfs  <<- "/Users/lavieestuntoucan/civ-sto/data/GTFS/sto/jan2017-july2017/"
-chipCards <<- "/Users/lavieestuntoucan/civ-sto/data/cartes_a_puces/sto/destination_estimee/Deplacement2017JanFev.CSV"
-routes.sto <<- c( 11, 17, 22, 23, 24, 25, 26, 27, 29, 31, 
-                  32, 33, 34, 35, 36, 37, 38, 40, 41, 44, 
-                  45, 46, 47, 55, 59, 67, 87, 85, 82, 94, 
-                  98, 93, 95, 300, 400, 200, 20 )
-trip.tag <<- "HIV"
-my.week  <<- c("Tuesday","Wednesday","Thursday")
-my.week.tag <<- "Semaine"
-output.dir <<- "/Users/lavieestuntoucan/civ-sto/tech/profil_de_charge/out/"
-first.day <<- as.Date("2017-01-09")
-last.day <<- as.Date("2017-02-26")
-#################
+set.user.data()
 
 ## Read gtfs data
 read.gtfs()
@@ -37,78 +25,147 @@ plot.freq()
 ## Select data period
 cut.data.period()
 
+## Fraction of trips with destination estimated
+frac.destOK()
+
 ## Merge of trips of a user
 merge.trips.info()
 
-## Loop over the routes
-for( i.route_id in routes.sto){
-  ##
-  ## Extract variantes
-  list.var <- extract_variantes(i.route_id)
-  for( i.var in list.var$trip_headsign){
-    cat(paste(i.route_id,i.var,"\n"))
-    ##
-    ## Extract the list of stops
-    list.stops <- extract_stops(i.route_id,i.var)
-    if( nrow(list.stops) == 0) next
-    if( nrow(list.stops) != length(unique(list.stops$stop_id)) ) next # no loops
-    ##
-    ## Extract trips on this line and variant
-    i.cards <- cards %>% filter(NoLigne == i.route_id & 
-                                  !is.na(NumArretDebarquement) &
-                                  NumArret %in% list.stops$stop_id &
-                                  NumArretDebarquement %in% list.stops$stop_id) %>% 
-      rowwise() %>% 
-      mutate(NumArretSeq = list.stops[which(list.stops$stop_id == NumArret),"stop_seq"],
-             NumArretDebarquementSeq = list.stops[which(list.stops$stop_id == NumArretDebarquement),"stop_seq"]) %>%
-      filter(NumArretDebarquementSeq > NumArretSeq)
-    ##
-    ## Number of in and out
-    df.stops.j <- list.stops %>% 
-      mutate(mont=NA,mont.tot=NA,des=NA,des.tot=NA,charge=NA) %>%
-      rowwise() %>% 
-      mutate(mont = length(which(i.cards$NumArret == stop_id)), 
-             des = length(which(i.cards$NumArretDebarquement == stop_id)))
-    ##
-    ## Total loading
-    df.stops.j$mont.tot <- cumsum(df.stops.j$mont)
-    df.stops.j$des.tot <- cumsum(df.stops.j$des)
-    df.stops.j$charge <- df.stops.j$mont.tot - df.stops.j$des.tot 
-    df.stops.j$stop_id <- factor(df.stops.j$stop_id, levels = df.stops.j$stop_id)
-    ##
-    ## Plot the whole
-    p <- plot_ly(df.stops.j, x = ~stop_id, y = ~mont, type = 'bar', name = 'Montées',hoverinfo = 'text',
-                 text = ~paste('Arrêt : ', stop_id,
-                               '</br>Nom :', stop_name,
-                               '</br>Montées : ', mont)) %>%
-      add_trace(y = ~-des, name = 'Descentes', hoverinfo = 'text',
-                text = ~paste('Arrêt : ', stop_id,
-                              '</br>Nom :', stop_name,
-                              '</br>Descentes : ', des)) %>%
-      add_trace(y = ~charge, name = 'Charge', type='scatter', mode = 'lines+markers',
-                hoverinfo = 'text',
-                text = ~paste('Arrêt : ', stop_id,
-                              '</br>Nom :', stop_name,
-                              '</br>Charge : ', charge))  %>%
-      layout(margin = list(l=50, r=50, b=100, t=50, pad=0),
-             yaxis = list(title = 'Décompte'), xaxis = list(title = "Numéro de l'arrêt"))
-    
-    out.html <- paste0(output.dir,"profilCharge_ligne",i.route_id,"_var",j,".html")
-    htmlwidgets::saveWidget(p, out.html)
-  }
+## Plot charge profile - 24h
+plot.charge.profiles()
+
+
+
+
+my.lines.1 <- c(22,25,26,29,40,41,44,45,46,47)
+my.lines.2 <- c(48)
+my.lines.3 <- c(11,17)
+my.lines.4 <- c(27)
+my.lines.5 <- c(85,88,93,94,95,98)
+my.lines.6 <- c(20,31,32,33)
+my.lines.7 <- c(23,24,34)
+my.lines.8 <- c(35,36,37,38)
+my.lines.9 <- c(55,59,67,200,400)
+
+my.stops.1 <- c(5048,5040,5032,5024,5025)
+my.stops.2 <- c(5026,5030,5034,5042,5050)
+my.stops.3 <- c(5014,5022,5030,5034,5042,5050)
+
+my.lines <- my.lines.9
+my.stops <- my.stops.3
+md <- "m"
+create.matrix.md(my.lines,my.stops,md)
+
+my.stops.1 <- c(5048,5040,5032,5024,5025)
+my.stops.2 <- c(5026,5030,5034,5042,5050)
+my.stops.3 <- c(5014,5022,5030,5034,5042,5050)
+stops.sto <- c(my.stops.1,my.stops.2,my.stops.3)
+
+round(cards %>% filter(NoLigne == 11 & NumArret %in% my.stops.2) %>% nrow() /21 / eff.destOK)
+round(cards %>% filter(NoLigne == 11 & NumArret %in% stops.sto) %>% nrow() /21 / eff.destOK)
+
+round(cards %>% filter(NoLigne == 11 & NumArret %in% stops.sto & !(NumArret %in% my.stops.2)) %>% nrow() /21 / eff.destOK)
+round(cards %>% filter(NoLigne == 11 & NumArretDebarquement %in% stops.sto & !(NumArret %in% my.stops.2)) %>% nrow() /21 / eff.destOK)
+cards %>% filter(NoLigne == 11 & NumArretDebarquement == 5022) %>% nrow() 
+
+round(cards %>% filter(NoLigne == 87 & NumArret %in% stops.sto) %>% nrow() /21 / eff.destOK)
+round(cards %>% filter(NoLigne == 87 & NumArretDebarquement %in% stops.sto) %>% nrow() /21 / eff.destOK)
+
+round(cards %>% filter(NumArret %in% stops.sto) %>% nrow() / 21 / eff.destOK)
+x <- cards %>% filter(NumArret %in% stops.sto)
+
+ggplot() +
+  geom_histogram(data=x,aes(x=HeureTransaction,y=7950*..count../sum(..count..)),binwidth = 1) +
+  scale_x_continuous(breaks = 0:25) +
+  scale_y_continuous(breaks=seq(0,2500,100))
+
+ggplot() +
+  geom_histogram(data=x,aes(x=HeureTransaction,y=100*..count../sum(..count..)),binwidth = 1) +
+  scale_x_continuous(breaks = 0:25) +
+  scale_y_continuous(breaks=seq(0,100,2))
+
+######
+create.matrix.md <- function(my.lines,my.stops,md){
+  m <- matrix(NA, ncol = length(my.lines), nrow = length(my.stops))
+  for( i in 1:length(my.lines)){
+    df <- count.md(my.lines[i],my.stops,md)
+    for( j in 1:length(my.stops) ){
+      if( !is.na(df)[1]){
+       n <- df %>% filter(stop_id == my.stops[j]) %>% select(count) %>% as.numeric()
+      }else{
+        n <- NA
+      }
+      m[j,i] <- n
+    }
+  } 
+  write.table(m,file = "/Users/lavieestuntoucan/Desktop/temp.csv",sep=",",row.names = F,quote = F,col.names = F)
+  print(m)
+}
+##
+
+
+
+my.lines <- c(22,25,26,29,40,41,44,45,46,47,48,11,17,27,85,88,93,
+              94,95,98,20,31,32,33,23,24,34,35,36,37,38,55,59,67,87,200,300,400)
+my.stops <- c(5048,5040,5032,5024,5025,
+              5026,5030,5034,5042,5050,
+              5014,5022,5030,5034,5042,5050,5016)
+md <- "d"
+create.matrix.md(my.lines,my.stops,md)
+
+create.matrix.md <- function(my.lines,my.stops,md){
+  m <- matrix(NA, ncol = length(my.lines), nrow = length(my.stops))
+  for( i in 1:length(my.lines)){
+    df <- count.md(my.lines[i],my.stops,md)
+    for( j in 1:length(my.stops) ){
+      if( !is.na(df)[1]){
+        n <- df %>% filter(stop_id == my.stops[j]) %>% select(count) %>% unique() %>% as.numeric()
+      }else{
+        n <- NA
+      }
+      m[j,i] <- n
+    }
+  } 
+  m <- as.data.frame(m)
+  colnames(m) <- my.lines
+  m$stops <- my.stops
+  write.table(m,file = "/Users/lavieestuntoucan/Desktop/temp.csv",sep=",",quote = F,row.names = F)
+  print(m)
 }
 
 
 
+# 
+# create.matrix <- function(my.lines,my.stops,md){
+#   m <- matrix(NA, ncol = length(my.lines), nrow = length(my.stops))
+#   for( i in 1:length(my.lines)){
+# 
+#     if( nrow(i.var) == 1 ) df <- count.charge(my.lines[i],i.var$trip_headsign[1]) 
+#     if( nrow(i.var) > 1 ){
+#       datalist = list()
+#       for(j in 1:nrow(i.var)){
+#         datalist[[j]] <- count.charge(my.lines[i],i.var$trip_headsign[j]) 
+#       }
+#       df <- do.call(rbind, datalist)
+#     }
+#     for( j in 1:length(my.stops) ){
+#       if( !is.na(df)[1]){
+#         if( md == "d" ) n <- df %>% filter(stop_id == my.stops[j]) %>% select(des)
+#         if( md == "m" ) n <- df %>% filter(stop_id == my.stops[j]) %>% select(mont)
+#         colnames(n) <- "count"
+#         cat(paste(i,j,"\n"))
+#         print(n)
+#         if( nrow(n) == 1 ) n <- n$count
+#         if( nrow(n) > 1 ) n <- sum(n$count)
+#       }else{
+#         n <- NA
+#       }
+#       cat(paste(i,j,n,"\n"))
+#       m[j,i] <- n
+#     }
+#   } 
+#   write.table(m,file = "/Users/lavieestuntoucan/Desktop/temp.csv",sep=",",row.names = F,quote = F,col.names = F)
+#   print(m)
+# }
 
 ########################################################################
-
-# ggplot(data=df.stops.j) + 
-#    geom_bar(aes(stop_seq,mont),stat="identity",fill="red") +
-#    geom_bar(aes(stop_seq,-des),stat="identity",fill="blue") +
-#    geom_point(aes(x=stop_seq,y=charge),color="forestgreen",size=1) +
-#    geom_line(aes(x=stop_seq,y=charge),color="forestgreen",size=1) +
-#    scale_x_continuous(breaks=df.stops.j$stop_seq,labels = df.stops.j$stop_id) +
-#    theme(axis.text.x = element_text(angle=60, hjust=1)) +
-#    xlab("Numéro de l'arrêt") +
-#    ylab("Nombre de personnes")
